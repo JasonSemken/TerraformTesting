b55739642e96dcdd689271baa9bb2a09de8dda65
Jason Semken <jason.semken@gmail.com>
2022-05-31 20:33:13 +1000

    MultiVM Deploy Creation

--- VMDeployTest/main.tf
+++ VMDeployTest/main.tf
@@ -20,13 +20,13 @@ resource "azurerm_subnet" "frontendsubnet" {
   name                 = "frontendSubnet"
   resource_group_name  = data.azurerm_resource_group.ResGroup.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
-  address_prefix       = "10.0.1.0/24"
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "myvm1publicip" {
   name                = "pip1"
   location            = data.azurerm_resource_group.ResGroup.location
-  resource_group_name = azurerm_resource_group.ResGroup.name
+  resource_group_name = data.azurerm_resource_group.ResGroup.name
   allocation_method   = "Dynamic"
   sku                 = "Basic"
 }
